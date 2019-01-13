fa7b43523f7e818ee91f4a8f863f430b0c6a216f
David Whynot <davidmwhynot@gmail.com>
2019-01-13 14:33:22 -0500

    setup gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require('gulp');
+const compiler = require('webpack');
+const webpack = require('webpack-stream');
+const path = require('path');
+const webpackConfig = require('./webpack.config');
+
+gulp.task('app',
+	() => gulp.src('src/public/js/app/index.js').pipe(webpack(webpackConfig, compiler)).pipe('dist/public'));
+
+gulp.task('default',
+	gulp.series('app', () => {
+		gulp.watch('src/public/js/app/**/*.js', gulp.series('app'));
+	}));
