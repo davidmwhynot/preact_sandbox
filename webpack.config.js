9b9b6a5f074cabab46546cb79c7ada12acfaee6c
David Whynot <davidmwhynot@gmail.com>
2019-01-13 15:06:58 -0500

    adding necessary rule for transpiling jsx

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,17 @@
 module.exports = {
-	rules: [
-		{ test: /\.css$/, loader: 'style!css' },
-	],
-	output: { filename: 'app.js' }
+	output: { filename: 'app.js' },
+	module: {
+		rules: [
+			{
+				test: /\.jsx?/i,
+				loader: 'babel-loader',
+				options: {
+					presets: ['es2015'],
+					plugins: [
+						['transform-react-jsx', { pragma: 'h' }]
+					]
+				}
+			}
+		]
+	}
 }
